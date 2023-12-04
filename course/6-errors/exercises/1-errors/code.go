fbf2cdc67a6a3aac895e065fc0981eed0f94667a
Luqman Rasat <luqmanrasat@hotmail.com>
2023-12-05 04:00:49 +0800

    ch 6 complete

--- course/6-errors/exercises/1-errors/code.go
+++ course/6-errors/exercises/1-errors/code.go
@@ -5,7 +5,17 @@ import (
 )
 
 func sendSMSToCouple(msgToCustomer, msgToSpouse string) (float64, error) {
-	// ?
+	costCustomer, err := sendSMS(msgToCustomer)
+	if err != nil {
+		return 0.0, err
+	}
+
+	costSpouse, err := sendSMS(msgToSpouse)
+	if err != nil {
+		return 0.0, err
+	}
+
+	return costCustomer + costSpouse, nil
 }
 
 // don't edit below this line
